20bca0d4e240cdcc648f49a1c843ee32eed180ca
liweisheng <1904792432@qq.com>
2019-04-04 11:23:38 +0800

    完成最坏情况下为线性复杂度的选择算法

--- Select.h
+++ Select.h
@@ -2,7 +2,7 @@
 #define SELECT_HPP_INCLUDED
 /**求中位数**/
 int MEDIAN(int *A,int low,int high);
-/**根据这个中位数进行partion**/
+/**根据这个中位数进行Partion**/
 int PARTION(int *A,int low,int high,int mid);
 /**总控递归程序**/
 int SELECT(int *A,int low,int high,int i);
